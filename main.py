994f75a72427b916625af1235c92a3c8108cec79
3akbolk <zach.wolf.tattoo@gmail.com>
2025-11-30 01:53:53 -0500

    finishing up

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 from stats import count_letters
 from stats import count_words
+import sys
+
+
+if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
 
 
 seperated_words = {}
 def test1():
-    word_count = count_words("/home/zakbolk/workspace/bookbot/books/frankenstein.txt")
+    word_count = count_words(sys.argv[1])
     print(f"Found {word_count} total words.")
     
     
-
-    
 def test2():
-    words = ("/home/zakbolk/workspace/bookbot/books/frankenstein.txt")
+    words = (sys.argv[1])
     seperated_words = count_letters(words)
     for char in seperated_words:
         print(char)
@@ -21,7 +25,6 @@ print("============ BOOKBOT ============")
 print("Analyzing book found at books/frankenstein.txt...")
 print("----------- Word Count ----------")
 test1()
-
 print("--------- Character Count -------")
 test2()
 print("============= END ===============")
